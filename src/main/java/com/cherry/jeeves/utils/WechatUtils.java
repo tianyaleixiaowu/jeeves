580cd111ac5892689df629df1634337d9df6f9d8
wuwf <272551766@qq.com>
2018-05-07 15:29:30 +0800

    加入异常捕获

--- src/main/java/com/cherry/jeeves/utils/WechatUtils.java
+++ src/main/java/com/cherry/jeeves/utils/WechatUtils.java
@@ -2,41 +2,48 @@ package com.cherry.jeeves.utils;
 
 import com.cherry.jeeves.domain.response.component.WechatHttpResponseBase;
 import com.cherry.jeeves.domain.shared.Contact;
-import com.cherry.jeeves.exception.WechatException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.nio.charset.StandardCharsets;
 
 public class WechatUtils {
+    private static Logger logger = LoggerFactory.getLogger("WechatUtils");
     public static void checkBaseResponse(WechatHttpResponseBase response) {
         if (response.getBaseResponse().getRet() != 0) {
-            throw new WechatException(response.getClass().getSimpleName() + " ret = " + response.getBaseResponse().getRet());
+            //throw new WechatException(response.getClass().getSimpleName() + " ret = " + response.getBaseResponse().getRet());
+            logger.error(response.getClass().getSimpleName() + " ret = " + response.getBaseResponse().getRet());
         }
     }
 
     public static String textDecode(String text) {
         if (text == null) {
-            throw new IllegalArgumentException("text");
+            //throw new IllegalArgumentException("text");
+            logger.error("text");
         }
         return new String(text.getBytes(StandardCharsets.ISO_8859_1), StandardCharsets.UTF_8);
     }
 
     public static boolean isIndividual(Contact contact) {
         if (contact == null) {
-            throw new IllegalArgumentException("contact");
+            //throw new IllegalArgumentException("contact");
+            logger.error("contact");
         }
         return contact.getUserName().startsWith("@") && !contact.getUserName().startsWith("@@") && ((contact.getVerifyFlag() & 8) == 0);
     }
 
     public static boolean isChatRoom(Contact contact) {
         if (contact == null) {
-            throw new IllegalArgumentException("contact");
+            //throw new IllegalArgumentException("contact");
+            logger.error("contact");
         }
         return contact.getUserName().startsWith("@@");
     }
 
     public static boolean isMediaPlatform(Contact contact) {
         if (contact == null) {
-            throw new IllegalArgumentException("contact");
+            //throw new IllegalArgumentException("contact");
+            logger.error("contact");
         }
         return contact.getUserName().startsWith("@") && !contact.getUserName().startsWith("@@") && ((contact.getVerifyFlag() & 8) > 0);
     }
